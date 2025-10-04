2b8fb382b8a1cdca6a91c0a2d4ecaf0e457fcd59
vedha <vedhanarayanan2002@gmail.com>
2025-10-04 16:50:28 +0530

    First commit for monkey

---
+++ token/token.go
@@ -0,0 +1,67 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN  = "="
+	PLUS    = "+"
+	MINUS   = "-"
+	BANG    = "!"
+	ASTERIK = "*"
+	SLASH   = "/"
+
+	// two letter operators
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	LT = "<"
+	GT = ">"
+
+	// Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	LET      = "LET"
+	FUNCTION = "FUNCTION"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+type Token struct {
+	Type    TokenType // what type this is a identifier,integer , keyword etc...
+	Literal string    // value of the literal like 5, 10 if integer
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}

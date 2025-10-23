e55f813471005838970f92b83398a31b01b493cf
vedha <vedhanarayanan2002@gmail.com>
2025-10-23 20:43:56 +0530

    prat-parser:: preparing the ast (expression node)

---
+++ ast/expression.go
@@ -0,0 +1,13 @@
+package ast
+
+import (
+	"monkey/token"
+)
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
